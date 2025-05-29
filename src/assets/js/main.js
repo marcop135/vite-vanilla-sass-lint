e26d6204dbaa65fda68a2457b5d535bcc2357239
marcop135 <marcop135.github@gmail.com>
2025-05-29 10:55:32 +0200

    enh: restructure HTML layout and enhance footer styling

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,6 +4,13 @@ import viteLogo from "../images/vite.svg";
 
 document.querySelector("#app").innerHTML = /*html*/ `
     <div class="container">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      <main>
+        <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      </main>
+      <footer>
+        <small>
+          This project uses the <a href="https://github.com/marcop135/vite-vanilla-sass-lint">vite-vanilla-sass-lint</a> starter by <a href="https://marcopontili.com" target="_blank" rel="noopener noreferrer">@marcop135</a>.
+        </small>
+      </footer>
     </div>
 `;
