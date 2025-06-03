8cbf593ff1f13caab0ba76faa5c8178f25a74fac
marcop135 <marcop135.github@gmail.com>
2025-06-03 16:31:10 +0200

    cho: minor demo UI tweak

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,15 +1,15 @@
-import "../scss/main.scss";
+import '../scss/main.scss';
 
-import viteLogo from "../images/vite.svg";
+import viteLogo from '../images/vite.svg';
 
-document.querySelector("#app").innerHTML = /*html*/ `
+document.querySelector('#app').innerHTML = /*html*/ `
     <div class="wrapper">
       <main>
         <img src="${viteLogo}" class="logo" alt="Vite logo" />
       </main>
       <footer>
         <small>
-          This project uses the <a href="https://github.com/marcop135/vite-vanilla-sass-lint">vite-vanilla-sass-lint</a> starter by <a href="https://marcopontili.com" target="_blank" rel="noopener noreferrer">@marcop135</a>.
+          This project uses the <a href="https://github.com/marcop135/vite-vanilla-sass-lint">vite-vanilla-sass-lint</a> starter.
         </small>
       </footer>
     </div>
