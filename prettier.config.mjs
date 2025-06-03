43d939000ccdc8dfb48bd75b53536a3e7d8d897f
marcop135 <marcop135.github@gmail.com>
2025-06-03 16:30:49 +0200

    feat: add support for prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('prettier').Config} */
+export default {
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 80,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+};
